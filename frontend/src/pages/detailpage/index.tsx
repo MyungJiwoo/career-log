7f1f19ceb452dc62b7c26621ec6ce75f6a70c922
MyungJiwoo <1206jiwoo@gmail.com>
2025-11-11 16:40:49 +0900

    Feat(FE): 지원한 공고 상세보기 GET 연결

--- frontend/src/pages/detailpage/index.tsx
+++ frontend/src/pages/detailpage/index.tsx
@@ -1,9 +1,68 @@
+import { BlockNoteView } from "@blocknote/mantine";
+import { useCreateBlockNote } from "@blocknote/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+interface Stage {
+  order: number;
+  name: string;
+  status: "pending" | "pass" | "nonpass";
+}
+
+interface AppliedJob {
+  number: number;
+  companyName: string;
+  position: string;
+  appliedDate: string;
+  stages: Stage[];
+  contents: string;
+  progress: "pending" | "in progress" | "completed";
+  createdAt: string;
+  updatedAt: string;
+}
+
 const DetailPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const [job, setJob] = useState<AppliedJob | undefined>();
+  const editor = useCreateBlockNote();
+
+  const fetchDetails = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/appliedJob/${id}`
+      );
+      setJob(response.data);
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.error("불러오기 실패:", error.response?.data || error.message);
+      alert("불러오기 중 오류가 발생했습니다.");
+    }
+  };
+
+  useEffect(() => {
+    fetchDetails();
+  }, []);
+
+  useEffect(() => {
+    if (!job?.contents) return;
+
+    try {
+      const parsedBlocks = JSON.parse(job.contents);
+      editor.replaceBlocks(editor.document, parsedBlocks);
+    } catch (error) {
+      console.error("BlockNote parsing failed:", error);
+    }
+  }, [job, editor]);
+
   return (
     <div className="flex flex-col flex-1">
       <div className="bg-white-100 rounded-2xl w-full p-8 gap-6 flex flex-col">
         <div className="flex justify-between items-center">
-          <h1 className="text-3xl font-bold text-black-900">기업 이름</h1>
+          <h1 className="text-3xl font-bold text-black-900">
+            {job?.companyName}
+          </h1>
           <button className="bg-white-100 text-white-700 w-fit py-1.5 px-3 rounded-xl cursor-pointer hover:bg-white-300">
             편집
           </button>
@@ -13,42 +72,29 @@ const DetailPage = () => {
           <div className="flex gap-12">
             <div className="flex flex-col gap-1">
               <p className="text-sm text-white-700">직무</p>
-              <p className="text-black-900">인턴</p>
+              <p className="text-black-900"> {job?.position}</p>
             </div>
 
             <div className="flex flex-col gap-1">
               <p className="text-sm text-white-700">지원일</p>
-              <p className="text-black-900">2025.11.11</p>
+              <p className="text-black-900"> {job?.appliedDate.slice(0, 10)}</p>
             </div>
           </div>
 
           <div className="flex flex-col gap-1.5">
             <p className="text-sm text-white-700">채용 절차</p>
             <div className="flex gap-2">
-              <p className="w-fit py-1 px-3 rounded-2xl bg-black-200 text-black-600 whitespace-nowrap">
-                서류
-              </p>
-              <p className="w-fit py-1 px-3 rounded-2xl bg-black-200 text-black-600 whitespace-nowrap">
-                인적성 검사
-              </p>
-              <p className="w-fit py-1 px-3 rounded-2xl bg-black-200 text-black-600 whitespace-nowrap">
-                코딩 테스트
-              </p>
-              <p className="w-fit py-1 px-3 rounded-2xl bg-black-200 text-black-600 whitespace-nowrap">
-                서류
-              </p>
-              <p className="w-fit py-1 px-3 rounded-2xl bg-black-200 text-black-600 whitespace-nowrap">
-                인적성 검사
-              </p>
-              <p className="w-fit py-1 px-3 rounded-2xl bg-black-200 text-black-600 whitespace-nowrap">
-                코딩 테스트
-              </p>
+              {job?.stages.map((stage) => (
+                <p className="w-fit py-1 px-3 rounded-2xl bg-black-200 text-black-600 whitespace-nowrap">
+                  {stage.name}
+                </p>
+              ))}
             </div>
           </div>
 
-          <div>
-            <p className="text-sm text-white-700">메모</p>
-            <div>이런 저런 이야기</div>
+          <div className="mt-10">
+            {/* <p className="text-sm text-white-700">메모</p> */}
+            <BlockNoteView editor={editor} editable={false} />
           </div>
         </div>
       </div>
--- frontend/src/pages/mainpage/index.tsx
+++ frontend/src/pages/mainpage/index.tsx
@@ -1,4 +1,24 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Stage {
+  order: number;
+  name: string;
+  status: "pending" | "pass" | "nonpass";
+}
+
+interface AppliedJob {
+  number: number;
+  companyName: string;
+  position: string;
+  appliedDate: string;
+  stages: Stage[];
+  contents: string;
+  progress: "pending" | "in progress" | "completed";
+  createdAt: string;
+  updatedAt: string;
+}
 
 const MainPage = () => {
   const navigate = useNavigate();
@@ -6,9 +26,26 @@ const MainPage = () => {
     navigate("/new");
   };
 
-  const navigateToDetail = () => {
-    navigate(`/100`);
+  const navigateToDetail = (number: number) => {
+    navigate(`/${number}`);
   };
+
+  const [jobs, setJobs] = useState<AppliedJob[] | undefined>();
+
+  const fetchAppliedJobs = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/appliedJob");
+      setJobs(response.data);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.error("불러오기 실패:", error.response?.data || error.message);
+      alert("불러오기 중 오류가 발생했습니다.");
+    }
+  };
+
+  useEffect(() => {
+    fetchAppliedJobs();
+  }, []);
 
   return (
     <div className="flex flex-col gap-6">
@@ -73,26 +110,26 @@ const MainPage = () => {
       <table className="w-full table-fixed border-separate bg-white-100 rounded-2xl p-4 border-spacing-x-4 border-spacing-y-3">
         <thead className="sticky top-0 z-10 text-black-600">
           <tr className="text-left">
-            <th scope="col" className="w-4 text-sm font-semibold">
+            <th scope="col" className="w-[5%] text-sm font-semibold">
               번호
             </th>
-            <th scope="col" className="w-10 text-sm font-semibold">
+            <th scope="col" className="w-[20%] text-sm font-semibold">
               기업명
             </th>
-            <th scope="col" className="w-8 text-sm font-semibold">
+            <th scope="col" className="w-[12%] text-sm font-semibold">
               직무
             </th>
-            <th scope="col" className="w-8 text-sm font-semibold">
+            <th scope="col" className="w-[13%] text-sm font-semibold">
               지원일
             </th>
-            <th scope="col" className="w-40 text-sm font-semibold">
+            <th scope="col" className="w-[50%] text-sm font-semibold">
               채용 절차
             </th>
           </tr>
         </thead>
 
         <tbody className="text-black-900">
-          <tr onClick={navigateToDetail} className="cursor-pointer">
+          {/* <tr onClick={navigateToDetail} className="cursor-pointer">
             <td>1</td>
             <td>기업 이름</td>
             <td>인턴</td>
@@ -119,26 +156,28 @@ const MainPage = () => {
                 </p>
               </div>
             </td>
-          </tr>
-          <tr onClick={navigateToDetail} className="cursor-pointer">
-            <td>2</td>
-            <td>기업 이름</td>
-            <td>인턴</td>
-            <td>2025.11.11</td>
-            <td>
-              <div className="flex gap-1.5 overflow-x-auto scrollbar-thin scrollbar-track-transparent">
-                <p className="text-sm w-fit py-1 px-3 rounded-2xl bg-black-200 text-black-600 whitespace-nowrap">
-                  서류
-                </p>
-                <p className="text-sm w-fit py-1 px-3 rounded-2xl bg-black-200 text-black-600 whitespace-nowrap">
-                  인적성 검사
-                </p>
-                <p className="text-sm w-fit py-1 px-3 rounded-2xl bg-black-200 text-black-600 whitespace-nowrap">
-                  코딩 테스트
-                </p>
-              </div>
-            </td>
-          </tr>
+          </tr> */}
+
+          {jobs?.map((job) => (
+            <tr
+              onClick={() => navigateToDetail(job.number)}
+              className="cursor-pointer"
+            >
+              <td>{job.number}</td>
+              <td>{job.companyName}</td>
+              <td>{job.position}</td>
+              <td>{job.appliedDate.slice(0, 10)}</td>
+              <td>
+                <div className="flex gap-1.5 overflow-x-auto scrollbar-thin scrollbar-track-transparent">
+                  {job.stages.map((stage) => (
+                    <p className="text-sm w-fit py-1 px-3 rounded-2xl bg-black-200 text-black-600 whitespace-nowrap">
+                      {stage.name}
+                    </p>
+                  ))}
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
