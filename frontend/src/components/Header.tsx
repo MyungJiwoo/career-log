6c6e92be88eb5884783b062ac47c642a65d29fbf
MyungJiwoo <1206jiwoo@gmail.com>
2025-11-12 19:18:11 +0900

    Fix(FE): 헤더의 z-index 수정

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-black-800 text-white-200 w-full h-[7vh] sticky top-0  flex items-center justify-center">
+    <header className="bg-black-800 text-white-200 w-full h-[7vh] sticky top-0 z-100 flex items-center justify-center">
       <nav className="max-w-240 w-full flex items-center justify-end gap-16">
         <NavLink to="/">지원 현황</NavLink>
         <p className="cursor-pointer">지원 예정</p>
