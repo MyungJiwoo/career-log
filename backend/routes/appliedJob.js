b125f8aafbe1b709b8eae14229f0d14edc0e6a82
MyungJiwoo <1206jiwoo@gmail.com>
2025-11-16 19:47:14 +0900

    Feat(BE): 지원 현황 통계 GET 구현

--- backend/routes/appliedJob.js
+++ backend/routes/appliedJob.js
@@ -45,6 +45,130 @@ router.get("/", async (req, res) => {
   }
 });
 
+/**
+ * 합격률 통계 api
+ */
+router.get("/statistics", async (req, res) => {
+  try {
+    // 전체
+    const totalApplications = await AppliedJob.countDocuments({});
+    const allStagePassedCount = await AppliedJob.countDocuments({
+      stages: {
+        $not: {
+          $elemMatch: { status: { $ne: "pass" } },
+        },
+      },
+    });
+    let totalPassRate = 0;
+    if (totalApplications > 0) {
+      totalPassRate = (allStagePassedCount / totalApplications) * 100;
+    }
+
+    // 서류
+    const totalDocumentApplications = await AppliedJob.countDocuments({
+      stages: {
+        $elemMatch: {
+          name: { $regex: /(서류|지원서|자기소개서|자소서)/ },
+        },
+      },
+    });
+    const documentPassedCount = await AppliedJob.countDocuments({
+      stages: {
+        $elemMatch: {
+          name: { $regex: /(서류|지원서|자기소개서|자소서)/ },
+          status: "pass",
+        },
+      },
+    });
+    let documentPassRate = 0;
+    if (totalDocumentApplications > 0) {
+      documentPassRate =
+        (documentPassedCount / totalDocumentApplications) * 100;
+    }
+
+    // 코딩 테스트
+    const totalCodingTestAttempts = await AppliedJob.countDocuments({
+      stages: {
+        $elemMatch: {
+          name: { $regex: /(코딩|코테)/ },
+        },
+      },
+    });
+    const codingTestPassedCount = await AppliedJob.countDocuments({
+      stages: {
+        $elemMatch: {
+          name: { $regex: /(코딩|코테)/ },
+          status: "pass",
+        },
+      },
+    });
+    let codingTestPassRate = 0;
+    if (totalCodingTestAttempts > 0) {
+      codingTestPassRate =
+        (codingTestPassedCount / totalCodingTestAttempts) * 100;
+    }
+
+    // 과제 테스트
+    const totalAssignmentAttempts = await AppliedJob.countDocuments({
+      stages: {
+        $elemMatch: {
+          name: { $regex: /(과제)/ },
+        },
+      },
+    });
+    const assignmentPassedCount = await AppliedJob.countDocuments({
+      stages: {
+        $elemMatch: {
+          name: { $regex: /(과제)/ },
+          status: "pass",
+        },
+      },
+    });
+    let assignmentPassRate = 0;
+    if (totalAssignmentAttempts > 0) {
+      assignmentPassRate =
+        (assignmentPassedCount / totalAssignmentAttempts) * 100;
+    }
+
+    // 면접
+    const totalInterviewAttempts = await AppliedJob.countDocuments({
+      stages: {
+        $elemMatch: {
+          name: { $regex: /(면접|인터뷰)/ },
+        },
+      },
+    });
+    const interviewPassedCount = await AppliedJob.countDocuments({
+      stages: {
+        $elemMatch: {
+          name: { $regex: /(면접|인터뷰)/ },
+          status: "pass",
+        },
+      },
+    });
+    let interviewPassRate = 0;
+    if (totalInterviewAttempts > 0) {
+      interviewPassRate = (interviewPassedCount / totalInterviewAttempts) * 100;
+    }
+
+    return res.json({
+      totalApplications,
+      totalPassRate,
+      totalDocumentApplications,
+      documentPassRate,
+      totalCodingTestAttempts,
+      codingTestPassRate,
+      totalAssignmentAttempts,
+      assignmentPassRate,
+      totalInterviewAttempts,
+      interviewPassRate,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "서버 오류가 발생했습니다." });
+  }
+});
+
 router.get("/:id", async (req, res) => {
   try {
     const job = await AppliedJob.findById(req.params.id);
